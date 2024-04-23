a395ccfc9b42aa0e88d5aaeccc5fe81cc27bfd2e
Luca Monteiro <lucamonteiro4@gmail.com>
2024-04-23 00:07:11 -0300

    Alert cardapio e correção

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// Verificação da idade
 function botao(){
     var idade = document.getElementById('idade').value;
     if (idade >= 18) {
-        window.location.href = "http://127.0.0.1:5501/index.html"
+        window.location.href = "http://127.0.0.1:5500/CP-JS2/home.html"
     }
     else {
         window.location.href = "https://site.mppr.mp.br/crianca/Pagina/Por-que-nao-se-deve-utilizar-o-termo-menor"
     }
+}
+
+// Alert do cardapio
+function adicionar(){
+    var adicionar = alert('Produto adicionado ao carrinho.')
 }
