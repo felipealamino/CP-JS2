a8b80508979737b0f950f469a576ae4ca799caab
Davi Vieira <davifmvieira@gmail.com>
2024-04-23 19:32:10 -0300

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function botao(){
     var idade = document.getElementById('idade').value;
     if (idade >= 18) {
-        window.location.href = "http://127.0.0.1:5500/CP-JS2/home.html"
+        window.location.href = "login.html"
     }
     else {
         window.location.href = "https://site.mppr.mp.br/crianca/Pagina/Por-que-nao-se-deve-utilizar-o-termo-menor"
@@ -18,7 +18,7 @@ function validarUsuario() {
     var usuario = document.getElementById('usuario').value;
     var senha = document.getElementById('senha').value;
     if (usuario === '1234' && senha === '1234') { 
-        window.location.href = 'home.html'
+        window.location.href = "home.html"
         alert('Logado com sucesso');
     } else {
         alert('Usuário ou senha incorretos');
