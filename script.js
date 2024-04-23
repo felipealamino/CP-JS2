f3ea7ff15b91ed18a5dce05f3e891b70a230e417
felipealamino <alaminofelipe67@gmail.com>
2024-04-23 13:17:52 -0300

    Página de login

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function botao(){
 function adicionar(){
     var adicionar = alert('Produto adicionado ao carrinho.')
 }
+
+function usuario(){
+    var usuario = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
+    if (usuario,senha === '1234'){
+        alert('Logado com sucesso')
+        window.location.href = "http://127.0.0.1:5500/CP-JS2/home.html"
+    }
+    else {
+        alert('Usuario ou Senha incorreta')
+        window.location.href = "https://support.google.com/youtube/answer/6009563?hl=pt-BR"
+    }
+}
