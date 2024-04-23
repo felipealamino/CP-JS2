535174d493024f4156c7a61ea04a2ac8ece10294
Luca Monteiro <lucamonteiro4@gmail.com>
2024-04-23 15:21:23 -0300

    painellogin e correções

--- script.js
+++ script.js
@@ -14,15 +14,13 @@ function adicionar(){
     var adicionar = alert('Produto adicionado ao carrinho.')
 }
 
-function usuario(){
+function validarUsuario() {
     var usuario = document.getElementById('usuario').value;
     var senha = document.getElementById('senha').value;
-    if (usuario,senha === '1234'){
-        alert('Logado com sucesso')
-        window.location.href = "http://127.0.0.1:5500/CP-JS2/home.html"
-    }
-    else {
-        alert('Usuario ou Senha incorreta')
-        window.location.href = "https://support.google.com/youtube/answer/6009563?hl=pt-BR"
+    if (usuario === '1234' && senha === '1234') { 
+        window.location.href = "http://127.0.0.1:5501/CP-JS2/home.html"
+        alert('Logado com sucesso');
+    } else {
+        alert('Usuário ou senha incorretos');
     }
 }
