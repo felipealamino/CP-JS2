0008838404281253f7618cfb7234d286ae681de5
Luca Monteiro <lucamonteiro4@gmail.com>
2024-04-24 19:51:55 -0300

    Ajustes + Formulario de contato

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function botao(){
     }
 }
 
+
+// Login
 function validarUsuario() {
     var usuario = document.getElementById('usuario').value;
     var senha = document.getElementById('senha').value;
@@ -18,4 +20,47 @@ function validarUsuario() {
     } else {
         alert('Usuário ou senha incorretos');
     }
+}
+
+
+// Carrinho e cupom
+let carrinho = [];
+    function adicionar(nome, preco) {
+        carrinho.push({ nome: nome, preco: preco });
+        alert("Item adicionado ao carrinho!");
+    }
+    function exibirCarrinho() {
+        let total = 0;
+        let mensagem = "Itens no carrinho:\n";
+
+        carrinho.forEach(function (item, index) {
+            mensagem += `${index + 1}. ${item.nome} - R$ ${item.preco}\n`;
+            total += item.preco;
+        });
+
+        let desconto = 0;
+    const cupom = prompt("Digite o cupom de desconto (se aplicável):");
+    if (cupom === "FIAP2024") {
+        desconto = total * 0.1; // 10% de desconto
+    } 
+    else if (mensagem) {
+        alert('Nenhum cupom aplicado');
+    }
+    
+    total -= desconto;
+
+    mensagem += `\nSubtotal: R$ ${total.toFixed(2)}`;
+
+    if (desconto > 0) {
+        mensagem += `\nDesconto (Cupom FIAP2024): -R$ ${desconto.toFixed(2)}`;
+    }
+
+    mensagem += `\nTotal: R$ ${(total).toFixed(2)}`;
+    alert(mensagem);
+}
+
+
+// Formulario de contato
+function contato(){
+    var contato = alert('Mensagem enviada com sucesso.')
 }
